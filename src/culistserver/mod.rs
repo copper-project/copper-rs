46e04a6c53e300610f4a6075ecb1b7fb6338d563
Guillaume Binet <gbin@gootz.net>
2020-09-07 21:18:46 -0400

    couple tasks communicating.

--- src/culistserver/mod.rs
+++ src/culistserver/mod.rs
@@ -6,7 +6,7 @@ use std::sync::Mutex;
 use crate::culist::CuList;
 use crate::culist::get_culist_from_shared_mem;
 
-const NB_LIST: usize = 32; // TODO: derive 32 from U32.
+pub const NB_LIST: usize = 32; // TODO: derive 32 from U32.
 
 
 type CuListLifoIds = CircularQueue<i32, U32>;
@@ -17,10 +17,6 @@ pub struct CuListSource {
     local_cus: Vec<*mut CuList>,
 }
 
-fn get_rawculistserver() -> &'static mut CuListSource {
-    let channel_id = get_culists_serving_channel_id();
-    return unsafe { &mut *(curpc::sharedmem::get_shared_mem(channel_id, size_of::<CuListSource>()) as *mut CuListSource) };
-}
 
 fn get_culists_serving_channel_id() -> i32 {
     return match shm::ftok!("./cubase/culists_serving_id\0") {
@@ -29,7 +25,7 @@ fn get_culists_serving_channel_id() -> i32 {
     };
 }
 
-fn get_base_channel_id() -> i32 {
+pub fn get_base_channel_id() -> i32 {
     return match shm::ftok!("./cubase/base_culist_id\0") {
         Some(id) => id as i32,
         None => -1,
@@ -38,7 +34,7 @@ fn get_base_channel_id() -> i32 {
 
 fn init_local_culist_source() -> CuListSource {
     let mut channel_id = get_culists_serving_channel_id();
-    let mut source: CuListSource;
+    let source: CuListSource;
     unsafe {
         source = CuListSource {
             guarded_shared_lifo: Mutex::new(&mut *(curpc::sharedmem::get_shared_mem(channel_id, size_of::<CuListLifoIds>()) as *mut CuListLifoIds)),
@@ -51,17 +47,18 @@ fn init_local_culist_source() -> CuListSource {
 
 pub(crate) fn get_culist_source() -> CuListSource {
     let mut source = init_local_culist_source();
-    source.sync_local_cus();
+    source.sync_local_culists();
     return source;
 }
 
 
 impl CuListSource {
-    fn sync_local_cus(&mut self) {
+    fn sync_local_culists(&mut self) {
         let mut id = self.base_id;
         for _ in 0..NB_LIST {
             let cu = get_culist_from_shared_mem(id);
             self.local_cus.push(cu);
+            cu.shared_mem_id = id;
             id += 1;
         }
     }
@@ -79,21 +76,23 @@ impl CuListSource {
     }
 
 
-    pub fn get_fresh_cu(&mut self) -> &mut CuList {
+    pub fn get_fresh_culist(&mut self) -> &mut CuList {
         let mutex = &mut self.guarded_shared_lifo;
         let result = mutex.lock();
         let mut lifo_ids = result.unwrap();
         let id = unsafe { *(**lifo_ids).pop().unwrap() };
-        //return self.get_cu(id);
+        println!("Fresh culist: poped {}", id);
+        let culist = unsafe { &mut *(self.local_cus[(id - self.base_id) as usize]) };
+        println!("Internal culist state is id {}", culist.shared_mem_id);
+        return culist;
+    }
+
+    pub fn get_culist(&mut self, id: i32) -> &mut CuList {
         return unsafe { &mut *(self.local_cus[(id - self.base_id) as usize]) };
     }
 
-    pub fn get_cu(&mut self, id: i32) -> &mut CuList {
-        return unsafe { &mut *(self.local_cus[(id - self.base_id) as usize]) };
-    }
-
-    pub fn recycle_cu(&mut self, id: i32) {
-        let cu : &mut CuList = self.get_cu(id);
+    pub fn recycle_culist(&mut self, id: i32) {
+        let cu : &mut CuList = self.get_culist(id);
         cu.reset();
         let mut lifo_ids = self.guarded_shared_lifo.lock().unwrap();
         unsafe { (**lifo_ids).push(id) };
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(min_const_generics)]
+#![feature(wrapping_int_impl)]
 
 extern crate nix;
 
@@ -7,128 +8,74 @@ use std::time::Instant;
 
 use nix::unistd::{fork, ForkResult};
 
-use crate::culistserver::get_culist_source;
+use crate::culistserver::{get_culist_source, get_base_channel_id, NB_LIST};
+use crate::cutask::{CuTask, CuTaskRunner, get_ctr};
+use crate::culist::{CuList, VALUE_SIZE, CuMsg};
+use crate::curpc::sharedmem::clean_up_existing_shared_mem;
+use std::num::Wrapping;
+use arrayvec::ArrayString;
+
 
 mod culist;
 mod curpc;
 mod culistserver;
 mod common;
+mod cutask;
 
 fn main() {
+    clean_up_existing_shared_mem(get_base_channel_id(), NB_LIST);
+    let mut cuserver = get_culist_source();
+    cuserver.start();
+
     match fork() {
         Ok(ForkResult::Parent { child, .. }) => {
             println!("Continuing execution in parent process, new child has pid: {}", child);
-            server();
+            thread::sleep(time::Duration::from_millis(1000));
         }
         Ok(ForkResult::Child) => {
-            println!("I'm a new child process");
-            client();
+            second_task();
         },
         Err(_) => println!("Fork failed"),
     }
-
-
-
-    thread::sleep(time::Duration::from_millis(1000));
-}
-
-
-fn server() {
-    //let path = setup();
-    //let origin_of_time = Instant::now();
-    //let channel_id = get_channel_id();
-    //println!("Channel ID: {:?}", channel_id);
-    //let cu = get_culist_from_shared_mem(&channel_id);
-    //emitter(origin_of_time, &path, cu);
-
-    let mut cuserver = get_culist_source();
-    cuserver.start();
-    thread::sleep(time::Duration::from_millis(1000));
-
-}
-
-fn client() {
-    //let path = setup();
-    //let origin_of_time = Instant::now();
-    //let channel_id = get_channel_id();
-    //println!("Channel ID: {:?}", channel_id);
-    //let cu = get_culist_from_shared_mem(&channel_id);
-    //receiver(origin_of_time, &path, cu);
-    let mut cuserver = get_culist_source();
-    for i in 0..40 {
-        let cu0id: i32;
-        let cu1id: i32;
-        {
-            let cu0 = cuserver.get_fresh_cu();
-            println!("{:?}", cu0);
-            cu0id =  cu0.shared_mem_id;
+    match fork() {
+        Ok(ForkResult::Parent { child, .. }) => {
+            println!("Continuing execution in parent process, new child has pid: {}", child);
         }
-        {
-            let cu1 = cuserver.get_fresh_cu();
-            println!("{:?}", cu1);
-            cu1id =  cu1.shared_mem_id;
-        }
-        cuserver.recycle_cu(cu0id);
-        cuserver.recycle_cu(cu1id);
-    }
-    thread::sleep(time::Duration::from_millis(1000));
-}
-
-
-
-/*
-fn get_ro_shared_mem(id: i32, size: usize) -> *const i32
-{
-    let id = match shm::shmget!(id, 0o0666 | shm::ffi::Ipc::CREAT as i32 | shm::ffi::Ipc::EXCL as i32,
-                                    size) {
-        Some(id) => id,
-        None => shm::shmget!( id, 0o0666, size).unwrap(),
-    };
-
-    let ptr = match shm::shmat!(id, std::ptr::null_mut(), 0) {
-        Some(ptr) => ptr as *const i32,
-        None => panic!("Could not attach to shared memory")
-    };
-    return ptr;
-}
-*/
-
-
-
-/*
-fn emitter(timeref: Instant, outgoing_fifo: &PathBuf, cu: &mut Cu){
-    let wave: [u8; 1] = [3];
-    let mut file = OpenOptions::new().write(true).open(outgoing_fifo).unwrap();
-
-    {
-        let mut test = cu.lock.lock();
-
-        // warn something is coming...
-        file.write(&wave).unwrap();
-
-        // here some business logic taking some time.
-        thread::sleep(time::Duration::from_millis(100));
-
-        cu.tov = Instant::now();
-        *test = cu.tov;
-        cu.value = (cu.tov.elapsed().as_nanos() % (255 as u128)) as i32;
-    }
-    println!("Value sent {}", cu.value)
-    // unlock the receiver
-}
-
-fn receiver(timeref: Instant, incoming_fifo: &PathBuf, cu: &mut Cu) {
-    let mut wave: &mut[u8; 1] = &mut [1];
-    let mut file = OpenOptions::new().read(true).open(incoming_fifo).unwrap();
-    match file.read(wave) {
-        Ok(n) => {
-            //println!("prepare to receive wave {:?}", wave[0]);
-            let l = cu.lock.lock();
-            println!("time: {:?}", cu.tov.elapsed());
-            println!("time locked: {:?}", l.elapsed());
-            println!("cu value: {:?}", cu.value);
+        Ok(ForkResult::Child) => {
+            first_task();
         },
-        Err(err) => println!("Error reading fifo: {}", err),
+        Err(_) => println!("Fork failed"),
+    }
+    thread::sleep(time::Duration::from_millis(1000));
+
+}
+
+struct FibonacciTask {}
+
+impl CuTask for FibonacciTask{
+    fn incoming_msg(&self, msg: &mut CuMsg) {
+        for i in 2..VALUE_SIZE-1 {
+           msg[i] = (Wrapping(msg[i-1]) + Wrapping(msg[i-2])).0;
+        }
     }
 }
-*/
+
+
+fn  first_task() {
+    let ctr = &mut get_ctr();
+    ctr.register_task(FibonacciTask{}, "first");
+    ctr.register_task(FibonacciTask{}, "second");
+    for _ in 0..10 {
+        ctr.run_first(0);
+    }
+}
+
+fn  second_task() {
+    let ctr = &mut get_ctr();
+    ctr.register_task(FibonacciTask{}, "first");
+    ctr.register_task(FibonacciTask{}, "second");
+    for _ in 0..10 {
+        ctr.run_middle(1);
+    }
+}
+
