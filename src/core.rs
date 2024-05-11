d3edeba2873ffa366a1e29b4dcba24a551107981
Guillaume Binet <gbin@gootz.net>
2024-05-11 12:16:07 -0400

    Pass on init and config for plugins

--- src/core.rs
+++ src/core.rs
@@ -1,19 +1,40 @@
 use std::sync::Mutex;
+use crate::config::ConfigNode;
+use crate::config::Value;
 
 lazy_static! {
-    pub static ref PLUGIN_REGISTRY: Mutex<Vec<Box<dyn Plugin>>> = Mutex::new(Vec::new());
+    pub static ref REGISTRY: Mutex<Vec<Box<dyn Plugin>>> = Mutex::new(Vec::new());
 }
 
-pub enum PluginType {
-    Driver,
-    Algorithm,
-    GPU
+pub trait CuTask {}
+
+// pub trait CuTaskConfig {
+//     fn get<T>(&self, key: &str) -> T;
+// }
+
+pub struct CuTaskConfig(ConfigNode);
+
+impl CuTaskConfig {
+    pub fn new(node: ConfigNode) -> Self {
+        CuTaskConfig(node)
+    }
+
+    pub fn get<T: std::convert::From<Value>>(&self, key: &str) -> Option<T> {
+        self.0.get_param(key)
+    }
 }
+
+type CuTaskCreator = dyn Fn(&CuTaskConfig) -> dyn CuTask;
+
+pub trait CuSource {}
+pub trait CuSink {}
+
+pub type CuError = Box<dyn std::error::Error>;
 
 pub trait Plugin : Send + Sync {
-    fn name(&self) -> &'static str;
-    fn plugin_type(&self) -> PluginType;
-    fn initialize(&self);
+    fn initialize(&self, cfg: CuTaskConfig) -> Result<(), CuError> {
+        Ok(())
+    }
 }
 
 #[macro_export]
@@ -21,7 +42,7 @@ macro_rules! register_plugin {
     ($plugin_ty:ty) => {
         pub fn _register_plugin() {
             let plugin: $plugin_ty = Default::default();
-            $crate::core::PLUGIN_REGISTRY.lock().unwrap().push(Box::new(plugin));
+            $crate::core::REGISTRY.lock().unwrap().push(Box::new(plugin));
         }
     };
 }
@@ -29,33 +50,28 @@ macro_rules! register_plugin {
 #[cfg(test)]
 mod tests {
     use crate::core::Plugin;
-    use crate::core::PluginType;
 
     #[derive(Default)]
     struct TestPlugin {}
 
-    impl Plugin for TestPlugin {
-        fn name(&self) -> &'static str {
-            "TestPlugin"
-        }
-
-        fn plugin_type(&self) -> PluginType {
-            PluginType::Driver
-        }
-
-        fn initialize(&self) {
-            println!("TestPlugin initialized");
-        }
-    }
+    impl Plugin for TestPlugin { }
 
     register_plugin!(TestPlugin);
 
     #[test]
     fn test_register_plugin() {
         _register_plugin();
-        let registry = crate::core::PLUGIN_REGISTRY.lock().unwrap();
+        let registry = crate::core::REGISTRY.lock().unwrap();
         assert_eq!(registry.len(), 1);
-        assert_eq!(registry[0].name(), "TestPlugin");
+    }
+
+    #[test]
+    fn test_plugin_config_accessor() {
+        let mut config = crate::config::ConfigNode::new("instanceName", "instanceType");
+        config.set_param("test", 42);
+        let task_config = crate::core::CuTaskConfig::new(config);
+        let value: i32 = task_config.get("test").unwrap();
+        assert_eq!(value, 42);
     }
 }
 
