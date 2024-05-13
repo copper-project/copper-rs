10baa45648e2bbca93f19e42b3aea74763530eb5
Guillaume Binet <gbin@gootz.net>
2024-05-13 09:50:09 -0400

    draft for the SrcTask

--- src/cutask.rs
+++ src/cutask.rs
@@ -1,9 +1,20 @@
 use crate::config::NodeConfig;
 use serde::{Deserialize, Serialize};
 
-trait CuStateful<'a>: Default + Serialize + Deserialize<'a> {}
+pub trait CuStateful<'a>: Default + Serialize + Deserialize<'a> {}
 
-trait CuTask<'a, I,O>: CuStateful<'a> {
+pub trait CuMsg<'a>: CuStateful<'a> {}
+
+pub trait CuSrcTask<'a, O>: CuStateful<'a> {
+    fn initialize(&self, config: NodeConfig, get_buffer: dyn Fn() -> &'a mut O, push_buffer: dyn Fn(&O));
+}
+
+pub trait CuTask<'a, I,O>: CuStateful<'a> {
     fn initialize(&self, config: NodeConfig);
     fn process(&self, input: &I, output: &O) -> Result<(), String>;
+}
+
+pub trait CuSinkTask<'a, I>: CuStateful<'a> {
+    fn initialize(&self, config: NodeConfig);
+    fn process(&self, input: &I) -> Result<(), String>;
 }
