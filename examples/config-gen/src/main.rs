17c6abf881fd8a08d982ace180782e529de05338
Guillaume Binet <gbin@gootz.net>
2024-05-21 10:41:20 -0400

    CopperList Generation
    Plus some refectoring work to make the circular queue work for the
    CopperLists.

--- examples/config-gen/src/main.rs
+++ examples/config-gen/src/main.rs
@@ -1,8 +1,8 @@
 use uom::si::time::second;
 use uom::si::time::Time;
 
-use copper::config::ConfigNode;
 use copper::config::CopperConfig;
+use copper::config::Node;
 use copper::config::Value;
 
 /// This is a simple example to show how to programmatically generate a configuration.
@@ -11,14 +11,12 @@ fn main() {
     // Generate a config
     let mut copperconfig = CopperConfig::default();
     let mut camera =
-        ConfigNode::new("camera", "camerapkg::Camera")
-            .set_base_period(Time::new::<second>(60.into()));
+        Node::new("camera", "camerapkg::Camera").set_base_period(Time::new::<second>(60.into()));
     camera.set_param::<Value>("resolution-height", 1080.into());
-    let isp =
-        ConfigNode::new("copper-isp", "isppkg::Isp").set_base_period(Time::new::<second>(1.into()));
+    let isp = Node::new("copper-isp", "isppkg::Isp").set_base_period(Time::new::<second>(1.into()));
     // isp.set_param::<Value>("tone", 1.3.into());
-    let algo = ConfigNode::new("copper-algo", "algopkg::Algo")
-        .set_base_period(Time::new::<second>(5.into()));
+    let algo =
+        Node::new("copper-algo", "algopkg::Algo").set_base_period(Time::new::<second>(5.into()));
     let n1 = copperconfig.add_node(isp);
     let n2 = copperconfig.add_node(camera);
     let n3 = copperconfig.add_node(algo);
