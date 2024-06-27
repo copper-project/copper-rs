d0a58fe6cf6d3b499c2bdf99fb649e60b7e244cc
Guillaume Binet <gbin@gootz.net>
2024-06-27 11:49:47 -0500

    datalogger -> unifiedlog

--- copper_log_test/src/main.rs
+++ copper_log_test/src/main.rs
@@ -1,28 +1,14 @@
-use copper::clock::RobotClock;
-use copper::DataLogType;
-use copper_datalogger::{stream_write, DataLogger, DataLoggerBuilder};
+use copper::monitoring::ScopedAllocCounter;
+use copper_helpers::basic_logger_runtime_setup;
 use copper_log_derive::debug;
-use copper_log_runtime::LoggerRuntime;
 use serde::Serialize;
 use std::path::PathBuf;
-use std::sync::{Arc, Mutex};
 
 fn main() {
-    let path: PathBuf = PathBuf::from("/tmp/teststructlog.copper");
-    let DataLogger::Write(logger) = DataLoggerBuilder::new()
-        .write(true)
-        .create(true)
-        .file_path(&path)
-        .preallocated_size(100000)
-        .build()
-        .expect("Failed to create logger")
-    else {
-        panic!("Failed to create logger")
-    };
-    let data_logger = Arc::new(Mutex::new(logger));
-    let clock = RobotClock::new();
-    let stream = stream_write(data_logger.clone(), DataLogType::StructuredLogLine, 1024);
-    let mut rt = LoggerRuntime::init(clock, stream, None);
+    let rt = basic_logger_runtime_setup(&PathBuf::from("/tmp/teststructlog.copper"), true)
+        .expect("Failed to setup logger.");
+    debug!("Logger created.");
+
     #[derive(Serialize)]
     struct Test {
         a: i32,
@@ -30,7 +16,7 @@ fn main() {
     }
     let mytuple = (1, "toto", 3.34f64, true, 'a');
     {
-        let hop = copper::monitoring::ScopedAllocCounter::new();
+        let hop = ScopedAllocCounter::new();
         let gigantic_vec = vec![0u8; 1_000_000];
         debug!("Just a string {}", "zarma");
         debug!("anonymous param constants {} {}", 42u16, 43u8);
@@ -39,6 +25,4 @@ fn main() {
         debug!("complex tuple", mytuple);
         debug!("Struct", Test { a: 3, b: 4 });
     }
-    debug!(" AFTER CLOSE {} ", "AFTER CLOSE");
-    rt.close();
 }
