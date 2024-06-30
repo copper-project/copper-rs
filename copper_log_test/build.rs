58eae7790825e69c659a20da54697108aa1868c4
Guillaume Binet <gbin@gootz.net>
2024-06-30 10:04:16 -0500

    Adds a CopperList holder + some tests checking if the serialization would work

--- copper_log_test/build.rs
+++ copper_log_test/build.rs
@@ -1,7 +1,5 @@
 use std::env;
 
-const INDEX_DIR_NAME: &str = "copper_log_index";
-
 fn main() {
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR is not defined");
     println!("cargo:rustc-env=OUT_DIR={}", out_dir);
