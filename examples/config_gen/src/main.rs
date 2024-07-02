f290c44b5f1278a49288f4efa44b5c7c2fb86fec
Guillaume Binet <gbin@gootz.net>
2024-07-02 15:34:53 -0500

    moar period fixes

--- examples/config_gen/src/main.rs
+++ examples/config_gen/src/main.rs
@@ -10,13 +10,11 @@ use copper::config::Value;
 fn main() {
     // Generate a config
     let mut copperconfig = CuConfig::default();
-    let mut camera =
-        Node::new("camera", "camerapkg::Camera").set_base_period(Time::new::<second>(60.into()));
+    let mut camera = Node::new("camera", "camerapkg::Camera");
     camera.set_param::<Value>("resolution-height", 1080.into());
-    let isp = Node::new("copper-isp", "isppkg::Isp").set_base_period(Time::new::<second>(1.into()));
+    let isp = Node::new("copper-isp", "isppkg::Isp");
     // isp.set_param::<Value>("tone", 1.3.into());
-    let algo =
-        Node::new("copper-algo", "algopkg::Algo").set_base_period(Time::new::<second>(5.into()));
+    let algo = Node::new("copper-algo", "algopkg::Algo");
     let n1 = copperconfig.add_node(isp);
     let n2 = copperconfig.add_node(camera);
     let n3 = copperconfig.add_node(algo);
