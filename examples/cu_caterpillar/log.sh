ecc90f1ded4ec40230f34331a18d36f2fad7643f
Guillaume Binet <gbin@gootz.net>
2024-07-04 23:04:29 -0500

    relative parth for log reading.

--- examples/cu_caterpillar/log.sh
+++ examples/cu_caterpillar/log.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-RUST_BACKTRACE=1 cargo run --bin logreader /tmp/caterpillar.copper extract-log /home/gbin/projects/copper/copper-project/target/debug/copper_log_index
+RUST_BACKTRACE=1 cargo run --bin logreader /tmp/caterpillar.copper extract-log ../../target/debug/copper_log_index
