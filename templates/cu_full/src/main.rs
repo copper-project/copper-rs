0bec308984d15f255d902f66b4c1bccf7030ffc8
Guillaume Binet <gbin@gootz.net>
2024-07-27 14:31:28 -0500

    cunew: Completed and fixed the cu_full example

--- templates/cu_full/src/main.rs
+++ templates/cu_full/src/main.rs
@@ -7,13 +7,15 @@ use std::path::PathBuf;
 use std::thread::sleep;
 use std::time::Duration;
 
+const PREALLOCATED_STORAGE_SIZE: Option<usize> = Some(1024 * 1024 * 100);
+
 #[copper_runtime(config = "copperconfig.ron")]
 struct {{project-name | upper_camel_case}}Application {}
 
 fn main() {
     let logger_path = "{{project-name | kebab_case}}.copper";
     let copper_ctx =
-        basic_copper_setup(&PathBuf::from(logger_path), true).expect("Failed to setup logger.");
+        basic_copper_setup(&PathBuf::from(logger_path), PREALLOCATED_STORAGE_SIZE, true).expect("Failed to setup logger.");
     debug!("Logger created at {}.", logger_path);
     let clock = copper_ctx.clock;
     debug!("Creating application... ");
--- templates/cu_full/src/tasks.rs
+++ templates/cu_full/src/tasks.rs
@@ -2,21 +2,28 @@ use bincode::de::Decoder;
 use bincode::enc::Encoder;
 use bincode::error::{DecodeError, EncodeError};
 use bincode::{Decode, Encode};
+
 use cu29::clock::{OptionCuTime, RobotClock};
-use cu29::cutask::{CuMsg, CuSrcTask, CuTask, CuTaskLifecycle, Freezable};
-use cu29::CuResult;
 use cu29::config::NodeInstanceConfig;
+use cu29::cutask::{CuMsg, CuSrcTask, CuSinkTask, CuTask, CuTaskLifecycle, Freezable};
+use cu29::CuResult;
+
+use cu29_log_derive::debug;
 
 
 // Define a message type
-#[derive(Debug, Clone, Encode, Decode)]
+#[derive(Default, Debug, Clone, Encode, Decode)]
 pub struct MyMsg {
     value: i32,
 }
 
+
 // Defines a source (ie. driver)
 #[derive(Default)]
 pub struct MySource {}
+
+// Needs to be fully implemented if you want to have a stateful task.
+impl Freezable for MySource {}
 
 impl CuTaskLifecycle for MySource {
     fn new(_config: Option<&NodeInstanceConfig>) -> CuResult<Self>
@@ -25,6 +32,7 @@ impl CuTaskLifecycle for MySource {
     {
         Ok(Self { })
     }
+    // don't forget the other lifecycle methods if you need them: start, stop, preprocess, postprocess
 }
 
 impl CuSrcTask for MySource {
@@ -37,16 +45,33 @@ impl CuSrcTask for MySource {
         };
         Ok(())
     }
+
 }
+
 
 // Defines a processing task
 pub struct MyTask {
     // if you add some task state here, you need to implement the Freezable trait
 }
 
-impl CuTask for CaterpillarTask {
+// Needs to be fully implemented if you want to have a stateful task.
+impl Freezable for MyTask {}
+
+impl CuTaskLifecycle for MyTask {
+    fn new(_config: Option<&NodeInstanceConfig>) -> CuResult<Self>
+    where
+        Self: Sized,
+    {
+        // add the task state initialization here
+        Ok(Self {})
+    }
+
+    // don't forget the other lifecycle methods if you need them: start, stop, preprocess, postprocess
+}
+
+impl CuTask for MyTask {
     type Input = MyMsg;
-    type Output = ();
+    type Output = MyMsg;
 
     fn process(
         &mut self,
@@ -54,8 +79,35 @@ impl CuTask for CaterpillarTask {
         input: &CuMsg<Self::Input>,
         output: &mut CuMsg<Self::Output>,
     ) -> CuResult<()> {
-        debug!("Received message: {}", input.payload);
-        Ok(())
+        debug!("Received message: {}", input.payload.value);
+        output.payload.value = 43;
+        Ok(()) // outputs another message for downstream
     }
 }
 
+// Defines a sink (ie. actualtion)
+#[derive(Default)]
+pub struct MySink {}
+
+// Needs to be fully implemented if you want to have a stateful task.
+impl Freezable for MySink {}
+
+impl CuTaskLifecycle for MySink {
+    fn new(_config: Option<&NodeInstanceConfig>) -> CuResult<Self>
+    where
+        Self: Sized,
+    {
+        Ok(Self { })
+    }
+    // don't forget the other lifecycle methods if you need them: start, stop, preprocess, postprocess
+}
+
+impl CuSinkTask for MySink {
+    type Input = MyMsg;
+
+    fn process(&mut self, _clock: &RobotClock, input: &mut CuMsg<Self::Input>) -> CuResult<()> {
+        debug!("Sink Received message: {}", input.payload.value);
+        Ok(())
+    }
+
+}
