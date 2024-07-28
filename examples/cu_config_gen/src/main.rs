a9dae02967d1bf701883e59fda6f8ed7bd673a63
Guillaume Binet <gbin@gootz.net>
2024-07-28 09:31:02 -0500

    all: makes the crate names consistent

--- examples/cu_config_gen/src/main.rs
+++ examples/cu_config_gen/src/main.rs
@@ -2,7 +2,7 @@ use cu29::config::CuConfig;
 use cu29::config::Node;
 use cu29::config::Value;
 
-/// This is a simple example to show how to programmatically generate a configuration.
+/// This is a small example to show how to programmatically generate a configuration.
 /// This is useful for making variations of your robot more easily.
 fn main() {
     // Generate a config
@@ -10,7 +10,6 @@ fn main() {
     let mut camera = Node::new("camera", "camerapkg::Camera");
     camera.set_param::<Value>("resolution-height", 1080.into());
     let isp = Node::new("copper-isp", "isppkg::Isp");
-    // isp.set_param::<Value>("tone", 1.3.into());
     let algo = Node::new("copper-algo", "algopkg::Algo");
     let n1 = copperconfig.add_node(isp);
     let n2 = copperconfig.add_node(camera);
@@ -18,5 +17,5 @@ fn main() {
 
     copperconfig.connect(n2, n1, "imgmsgpkg::Image");
     copperconfig.connect(n1, n3, "imgmsgpkg::Image");
-    // println!("{}", copperconfig.serialize());
+    println!("{}", copperconfig.serialize_ron());
 }
