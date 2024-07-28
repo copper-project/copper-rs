3baa94f2e00c7373d2c28dce668afcba4ef0f846
Guillaume Binet <gbin@gootz.net>
2024-07-28 09:31:02 -0500

    all: makes the crate names consistent

--- examples/cu_caterpillar/copper7.sh
+++ examples/cu_caterpillar/copper7.sh
@@ -1,2 +1,2 @@
 cross build --target armv7-unknown-linux-gnueabihf --release
-scp ../../target/armv7-unknown-linux-gnueabihf/release/cucaterpillar copper7:copper
+scp ../../target/armv7-unknown-linux-gnueabihf/release/cu-caterpillar copper7:copper
