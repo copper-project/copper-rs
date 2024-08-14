9e659823b896d5f1cf80581f3c312ff81e784ec5
Guillaume Binet <gbin@gootz.net>
2024-08-14 12:19:38 -0500

    template: missed a LOGIN_INDEX_DIR spot

--- templates/cu_full/build.rs
+++ templates/cu_full/build.rs
@@ -2,5 +2,5 @@ use std::env;
 fn main() {
     // This is essential to be able to generate the structure log index within the project out directory.
     let out_dir = env::var("OUT_DIR").unwrap();
-    println!("cargo:rustc-env=OUT_DIR={}", out_dir);
+    println!("cargo:rustc-env=LOGIN_INDEX_DIR={}", out_dir);
 }
