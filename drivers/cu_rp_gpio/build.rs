43e2fbdaaa9d6ac807df8dc6e7af9fd34c723a31
Guillaume Binet <gbin@gootz.net>
2024-08-23 13:27:05 -0500

    Adds macos to the CI/CD pipeline (#25)
    * Adds macos to the CI/CD pipeline
    
    * Try to cut off uncompilable dependencies on macos
    
    * python support for cu29-export on macos
    
    * another way of adding python + be sure the LD library paths are set
    
    * try to add some flags to rustc to fix the linking issue
    
    * trying fully with specific flags
    
    * still not sorking trying to switch to macos-13
    
    * try filtering our macos
    
    * continue to purge things to accomodate macos
    
    * The page size is maybe not 4096 on macos
    
    * filter out the example until it is lander on master

--- drivers/cu_rp_gpio/build.rs
+++ drivers/cu_rp_gpio/build.rs
@@ -3,4 +3,8 @@ fn main() {
         "cargo:rustc-env=LOG_INDEX_DIR={}",
         std::env::var("OUT_DIR").unwrap()
     );
+    /// completely fail if the platform is macos
+    if cfg!(target_os = "macos") {
+        panic!("macos is not supported");
+    };
 }
