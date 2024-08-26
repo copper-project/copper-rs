9a180c6abc5f1a843c04b561c1f5ac5e6267f200
Guillaume Binet <gbin@gootz.net>
2024-08-26 09:56:32 -0500

    nit: small warning.

--- drivers/cu_rp_gpio/build.rs
+++ drivers/cu_rp_gpio/build.rs
@@ -3,7 +3,7 @@ fn main() {
         "cargo:rustc-env=LOG_INDEX_DIR={}",
         std::env::var("OUT_DIR").unwrap()
     );
-    /// completely fail if the platform is macos
+    // completely fail if the platform is macos
     if cfg!(target_os = "macos") {
         panic!("macos is not supported");
     };
