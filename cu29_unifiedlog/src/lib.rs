4e4513ad850427b034781f4c0a66da6453724b3f
Guillaume Binet <gbin@gootz.net>
2024-08-28 13:56:08 -0500

    Workaround for the flush before write on section header.

--- cu29_unifiedlog/src/lib.rs
+++ cu29_unifiedlog/src/lib.rs
@@ -417,8 +417,9 @@ impl SectionHandle {
         }
         self.section_header.filled_size = self.used;
 
-        let _sz = encode_into_slice(&self.section_header, &mut self.buffer, standard())
-            .expect("Failed to encode section header");
+        // FIX ME: This was flushed before and cannot be written back to.
+        // let _sz = encode_into_slice(&self.section_header, &mut self.buffer, standard())
+        //     .expect("Failed to encode section header");
     }
 }
 
