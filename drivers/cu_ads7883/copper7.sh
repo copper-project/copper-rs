b316fbd8cbd499837db03e1f26180456e1276b42
Guillaume Binet <gbin@gootz.net>
2024-09-09 13:23:29 -0500

    copies also the config to the target

--- drivers/cu_ads7883/copper7.sh
+++ drivers/cu_ads7883/copper7.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# cargo build --release --target arm-unknown-linux-musleabihf
 EXEC=$(cargo test --message-format=json --target arm-unknown-linux-musleabihf --no-run | jq -r 'select(.executable and .target.kind[] == "test") | .executable')
 echo 'This is the executable:' $EXEC
-scp $EXEC copper7:ads7883test
-scp -r /home/gbin/projects/copper/copper-rs/target/arm-unknown-linux-musleabihf/debug/cu29_log_index copper7:
+scp $EXEC copper7:testads7883
+scp tests/copperconfig.ron copper7:tests/copperconfig.ron
