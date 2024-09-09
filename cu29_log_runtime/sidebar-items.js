c19eaf92f8fc02069c81effae20a0c02ab7c508e
gbin <gbin@users.noreply.github.com>
2024-09-09 18:02:00 +0000

    deploy: 7acd3e0db58a52a18b7df35018b301e4a07536df

--- cu29_log_runtime/sidebar-items.js
+++ cu29_log_runtime/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["log"],"struct":["ExtraTextLogger","LoggerRuntime","OwningIoWriter","SimpleFileWriter"]};
+window.SIDEBAR_ITEMS = {"fn":["log","log_debug_mode"],"struct":["LoggerRuntime","NullLog","OwningIoWriter","SimpleFileWriter"]};
---
+++ trait.impl/log/trait.Log.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["cu29_log_runtime",[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.22/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"cu29_log_runtime/struct.NullLog.html\" title=\"struct cu29_log_runtime::NullLog\">NullLog</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[263]}
