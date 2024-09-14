ec1fd2f533041fff3c14d8882ed5aa764c6eabaa
gbin <gbin@users.noreply.github.com>
2024-09-14 18:11:45 +0000

    deploy: 59fcaf786cc6029adaab272c33f06346c4cbfffe

--- trait.impl/cu29_traits/trait.WriteStream.js
+++ trait.impl/cu29_traits/trait.WriteStream.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cu29",[]],["cu29_log_runtime",[["impl <a class=\"trait\" href=\"cu29_traits/trait.WriteStream.html\" title=\"trait cu29_traits::WriteStream\">WriteStream</a>&lt;<a class=\"struct\" href=\"cu29_log/struct.CuLogEntry.html\" title=\"struct cu29_log::CuLogEntry\">CuLogEntry</a>&gt; for <a class=\"struct\" href=\"cu29_log_runtime/struct.SimpleFileWriter.html\" title=\"struct cu29_log_runtime::SimpleFileWriter\">SimpleFileWriter</a>"]]]]);
+    var implementors = Object.fromEntries([["cu29",[]],["cu29_log_runtime",[["impl WriteStream&lt;CuLogEntry&gt; for <a class=\"struct\" href=\"cu29_log_runtime/struct.SimpleFileWriter.html\" title=\"struct cu29_log_runtime::SimpleFileWriter\">SimpleFileWriter</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[11,425]}
+//{"start":57,"fragment_lengths":[11,214]}
