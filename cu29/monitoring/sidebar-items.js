e1015d7db21d78716712db1ab2da6d7ad485ece0
gbin <gbin@users.noreply.github.com>
2024-09-28 15:11:31 +0000

    deploy: 365eaccf620c1561acdb15f043ec878d1b73bd40

--- cu29/monitoring/sidebar-items.js
+++ cu29/monitoring/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CuTaskState","Decision"],"static":["GLOBAL"],"struct":["CountingAllocator","ScopedAllocCounter"],"trait":["CuMonitor"]};
+window.SIDEBAR_ITEMS = {"enum":["CuTaskState","Decision"],"static":["GLOBAL"],"struct":["CountingAllocator","CuDurationStatistics","LiveStatistics","ScopedAllocCounter"],"trait":["CuMonitor"]};
