3e9b2c0143a90dfac77635a5099c586aecb23b03
Guillaume Binet <gbin@gootz.net>
2024-09-28 13:49:30 -0500

    A missing payload -> tuple

--- templates/cu_full/src/logreader.rs
+++ templates/cu_full/src/logreader.rs
@@ -1,11 +1,11 @@
 pub mod tasks;
 
 use cu29::cutask::CuMsg as _CuMsg;
-use cu29_derive::gen_culist_payload;
+use cu29_derive::gen_culist_tuple;
 use cu29_export::run_cli;
 
-type CuListPayload = gen_culist_payload!("copperconfig.ron");
+type CuListTuple = gen_culist_tuple!("copperconfig.ron");
 
 fn main() {
-    run_cli::<CuListPayload>().expect("Failed to run the export CLI");
+    run_cli::<CuListTuple>().expect("Failed to run the export CLI");
 }
