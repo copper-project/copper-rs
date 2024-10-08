01772b292d1c9298c04c18f4c7c74530aab71464
gbin <gbin@users.noreply.github.com>
2024-10-08 16:55:18 +0000

    deploy: 7cb3a9a99ff77c7ff976b5af1c836803e687fa65

---
+++ trait.impl/core/ops/arith/trait.AddAssign.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["cu29_clock",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a> for <a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[303]}
