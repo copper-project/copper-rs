67ba7d120e5a657c5b5fc2753e2a70240f2c11db
gbin <gbin@users.noreply.github.com>
2024-10-12 15:03:47 +0000

    deploy: 11c0567b2df17c8f534598114530f9c0ec87de00

---
+++ trait.impl/core/ops/arith/trait.Mul.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["cu29_clock",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.i32.html\">i32</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u32.html\">u32</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>"],["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;T&gt; for <a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a><div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>&gt;,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1703]}
