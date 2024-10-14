20dda7bd6751fd5cd3c3dfa070e2efa2c756987d
Massi <massi.amrouche314@gmail.com>
2024-10-13 18:07:34 -0700

    [Docker] Fix the mount docker in docker to include the full workspace

--- support/docker/run_docker.sh
+++ support/docker/run_docker.sh
@@ -2,4 +2,4 @@
 # Run the cooper-rs-env container and mount this directory in the container
 # Should make this a volume mount eventually
 # Should be run from within the "docker" directory for the mounting to work properly
-docker run -it -entrypoint="" --mount type=bind,source="$(pwd)"/..,target=/home/copper-rs copper-rs-env 
+docker run -it -entrypoint="" --mount type=bind,source="$(pwd)"/../..,target=/home/copper-rs copper-rs-env 
