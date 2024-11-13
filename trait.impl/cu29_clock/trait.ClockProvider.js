376f4477729ec4ce6ac04c342d11bd15a7d43247
gbin <gbin@users.noreply.github.com>
2024-11-13 16:26:43 +0000

    deploy: 162a31105dd0482d766340b317e1598730a06878

--- trait.impl/cu29_clock/trait.ClockProvider.js
+++ trait.impl/cu29_clock/trait.ClockProvider.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cu29",[["impl&lt;CT, P: <a class=\"trait\" href=\"cu29/trait.CopperListTuple.html\" title=\"trait cu29::CopperListTuple\">CopperListTuple</a>, M: <a class=\"trait\" href=\"cu29/monitoring/trait.CuMonitor.html\" title=\"trait cu29::monitoring::CuMonitor\">CuMonitor</a>, const NBCL: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; ClockProvider for <a class=\"struct\" href=\"cu29/curuntime/struct.CuRuntime.html\" title=\"struct cu29::curuntime::CuRuntime\">CuRuntime</a>&lt;CT, P, M, NBCL&gt;"]]]]);
+    var implementors = Object.fromEntries([["cu29",[["impl&lt;CT, P: <a class=\"trait\" href=\"cu29/trait.CopperListTuple.html\" title=\"trait cu29::CopperListTuple\">CopperListTuple</a>, M: <a class=\"trait\" href=\"cu29/monitoring/trait.CuMonitor.html\" title=\"trait cu29::monitoring::CuMonitor\">CuMonitor</a>, const NBCL: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"cu29_clock/trait.ClockProvider.html\" title=\"trait cu29_clock::ClockProvider\">ClockProvider</a> for <a class=\"struct\" href=\"cu29/curuntime/struct.CuRuntime.html\" title=\"struct cu29::curuntime::CuRuntime\">CuRuntime</a>&lt;CT, P, M, NBCL&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[556]}
+//{"start":57,"fragment_lengths":[666]}
