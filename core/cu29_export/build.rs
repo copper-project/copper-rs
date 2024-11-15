10a0bd79dbd50502d7cea351a4a30855acc4ee28
Guillaume Binet <gbin@gootz.net>
2024-11-15 10:56:33 -0600

    Various publish fixes for issues I hit trying to use cargo workspace (#103)
    * Various publish fixes
    
    * Fixes #70

--- core/cu29_export/build.rs
+++ core/cu29_export/build.rs
@@ -1,23 +1,25 @@
-use std::env;
-use std::fs;
-use std::path::PathBuf;
+// use std::env;
+// use std::fs;
+// use std::path::PathBuf;
 
 fn main() {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let target_dir = out_dir.join("../../../../..");
-    let lib_name = "libcu29_export.so";
-    let new_name = "cu29_export.so";
+    // FIXME: This is freaking out the crate publishing for good reasons.
 
-    let lib_path = target_dir.join(lib_name);
-    let new_path = target_dir.join(new_name);
+    // let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    // let target_dir = out_dir.join("../../../../..");
+    // let lib_name = "libcu29_export.so";
+    // let new_name = "cu29_export.so";
 
-    if let Ok(existing_link) = fs::read_link(&new_path) {
-        if existing_link != lib_path {
-            fs::remove_file(&new_path).expect("Failed to remove existing symlink");
-        } else {
-            return;
-        }
-    }
+    // let lib_path = target_dir.join(lib_name);
+    // let new_path = target_dir.join(new_name);
 
-    std::os::unix::fs::symlink(&lib_path, &new_path).expect("Failed to create symlink");
+    // if let Ok(existing_link) = fs::read_link(&new_path) {
+    //     if existing_link != lib_path {
+    //         fs::remove_file(&new_path).expect("Failed to remove existing symlink");
+    //     } else {
+    //         return;
+    //     }
+    // }
+
+    // std::os::unix::fs::symlink(&lib_path, &new_path).expect("Failed to create symlink");
 }
--- core/cu29_log_runtime/README.md
+++ core/cu29_log_runtime/README.md
@@ -1,8 +1,8 @@
 ## Structure log runtime
 
 This is a supporting crate
-for [cu29_log_derive](https://github.com/copper-project/copper-rs/tree/master/cu29_log_derive), check this crate for the
-actual usage.
+for [cu29_log_derive](https://github.com/copper-project/copper-rs/tree/master/core/cu29_log_derive), check this crate
+for the actual usage.
 
 This crate provides the runtime part that is called my the macros like `!debug`.
 
