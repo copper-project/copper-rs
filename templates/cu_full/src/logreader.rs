5dfa28e8163665760705b70687000169957578f7
Guillaume Binet <gbin@gootz.net>
2024-12-02 08:31:03 -0600

    quick fix on project generation.
    Added crate:: for cumsgs generation.

--- templates/cu_full/src/logreader.rs
+++ templates/cu_full/src/logreader.rs
@@ -1,11 +1,10 @@
 pub mod tasks;
 
-use cu29::cutask::CuMsg as _CuMsg;
 use cu29_derive::gen_cumsgs;
 use cu29_export::run_cli;
 
-/// This will create the CuMsgs that is specific to your copper project.
-/// It is used to instruct the log reader how to decode the logs.
+// This will create the CuMsgs that is specific to your copper project.
+// It is used to instruct the log reader how to decode the logs.
 gen_cumsgs!("copperconfig.ron");
 
 fn main() {
