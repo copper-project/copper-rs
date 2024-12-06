70a0cfc35edbe59626ff90292bb7c29437f2973f
Guillaume Binet <gbin@gootz.net>
2024-12-06 08:55:25 -0600

    prelude itself

---
+++ core/cu29_prelude/src/lib.rs
@@ -0,0 +1,12 @@
+#![doc = include_str!("../README.md")]
+
+pub use bincode;
+pub use cu29::*;
+pub use cu29_derive::*;
+pub use cu29_helpers::*;
+pub use cu29_log::*;
+pub use cu29_log_derive::*;
+pub use cu29_log_runtime::*;
+#[allow(unused_imports)]
+pub use cu29_traits::*;
+pub use cu29_unifiedlog::*;
