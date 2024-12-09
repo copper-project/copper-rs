db0a66e9083c3c79897a411e2105aca30a835843
Guillaume Binet <gbin@gootz.net>
2024-12-09 13:59:53 -0600

    Split cu29 to cu29 (prelude) and cu29_runtime (actual impl)

--- core/cu29/src/lib.rs
+++ core/cu29/src/lib.rs
@@ -1,13 +1,34 @@
 #![doc = include_str!("../README.md")]
 
-pub mod config;
-pub mod copperlist;
-pub mod curuntime;
-pub mod cutask;
-pub mod monitoring;
-pub mod payload;
-pub mod simulation;
+// backward compatibility
+pub use cu29_runtime::config;
+pub use cu29_runtime::copperlist;
+pub use cu29_runtime::curuntime;
+pub use cu29_runtime::cutask;
+pub use cu29_runtime::input_msg;
+pub use cu29_runtime::monitoring;
+pub use cu29_runtime::output_msg;
+pub use cu29_runtime::payload;
+pub use cu29_runtime::simulation;
 
-pub use config::read_configuration;
 pub use cu29_clock as clock;
+pub use cu29_runtime::config::read_configuration;
 pub use cu29_traits::*;
+
+pub mod prelude {
+    pub use cu29_clock as clock;
+    pub use cu29_log::*;
+    pub use cu29_log_derive::*;
+    pub use cu29_log_runtime::*;
+    pub use cu29_runtime::config::*;
+    pub use cu29_runtime::copperlist::*;
+    pub use cu29_runtime::curuntime::*;
+    pub use cu29_runtime::cutask::*;
+    pub use cu29_runtime::input_msg;
+    pub use cu29_runtime::monitoring::*;
+    pub use cu29_runtime::output_msg;
+    pub use cu29_runtime::payload::*;
+    pub use cu29_runtime::simulation::*;
+    pub use cu29_runtime::*;
+    pub use cu29_traits::*;
+}
