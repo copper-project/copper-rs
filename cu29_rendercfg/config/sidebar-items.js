0c2868fe86d24c696ce64597c0f217c462f5b909
gbin <gbin@users.noreply.github.com>
2024-12-30 12:53:37 +0000

    deploy: 3f240329eb026c062371803a126c0585efcb43f1

--- cu29_rendercfg/config/sidebar-items.js
+++ cu29_rendercfg/config/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["read_configuration","read_configuration_str"],"struct":["Cnx","ComponentConfig","CuConfig","CuConfigRepresentation","MonitorConfig","Node","Value"],"type":["NodeId"]};
+window.SIDEBAR_ITEMS = {"fn":["read_configuration","read_configuration_str"],"struct":["Cnx","ComponentConfig","CuConfig","CuConfigRepresentation","LoggingConfig","MonitorConfig","Node","Value"],"type":["NodeId"]};
