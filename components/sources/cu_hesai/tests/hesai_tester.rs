d3845614bd855398312cbf94c86c1ef0beac0013
Luc <luc.frauciel@konboi.one>
2025-01-07 19:33:41 +0000

    Fix tmp path

--- components/sources/cu_hesai/tests/hesai_tester.rs
+++ components/sources/cu_hesai/tests/hesai_tester.rs
@@ -69,8 +69,10 @@ struct HesaiTester {}
 
 #[test]
 fn main() {
-    let logger_path = "/tmp/hesai_tester.copper";
-    let copper_ctx = basic_copper_setup(&PathBuf::from(logger_path), None, true, None)
+    let tmp_dir = tempfile::TempDir::new().expect("could not create a tmp dir");
+    let logger_path = tmp_dir.path().join("hesai_tester.copper");
+
+    let copper_ctx = basic_copper_setup(&PathBuf::from(&logger_path), None, true, None)
         .expect("Failed to setup logger.");
     debug!("Logger created at {}.", logger_path);
 
