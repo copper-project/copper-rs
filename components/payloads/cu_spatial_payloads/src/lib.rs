ebfa0ccc57233cde46987afbccadb0f3f5886978
Guillaume Binet <gbin@gootz.net>
2025-02-28 11:44:46 -0600

    Generalizes it at least to 32 and 65 floats + uom

--- components/payloads/cu_spatial_payloads/src/lib.rs
+++ components/payloads/cu_spatial_payloads/src/lib.rs
@@ -1,31 +1,83 @@
 use bincode::{Decode, Encode};
 use core::fmt::Debug;
+use uom::si::angle::radian;
+use uom::si::length::meter;
+pub type Pose<T> = Transform3D<T>;
+use uom::si::f32::Angle as Angle32;
+use uom::si::f32::Length as Length32;
+use uom::si::f64::Angle as Angle64;
+use uom::si::f64::Length as Length64;
 
-pub type Pose = Transform3D;
-
-/// Implements a transform that can be shipped as a Copper payload.
 #[derive(Debug, Clone, Encode, Decode)]
-pub struct Transform3D {
-    pub mat: [[f64; 4]; 4],
+pub struct Transform3D<T: Copy + Debug + 'static> {
+    pub mat: [[T; 4]; 4],
 }
 
-impl Transform3D {
-    pub fn translation(&self) -> [f64; 3] {
-        [self.mat[0][3], self.mat[1][3], self.mat[2][3]]
+impl Transform3D<f64> {
+    pub fn translation(&self) -> [Length64; 3] {
+        [
+            Length64::new::<meter>(self.mat[0][3]),
+            Length64::new::<meter>(self.mat[1][3]),
+            Length64::new::<meter>(self.mat[2][3]),
+        ]
     }
 
-    pub fn rotation(&self) -> [[f64; 3]; 3] {
+    pub fn rotation(&self) -> [[Angle64; 3]; 3] {
         [
-            [self.mat[0][0], self.mat[0][1], self.mat[0][2]],
-            [self.mat[1][0], self.mat[1][1], self.mat[1][2]],
-            [self.mat[2][0], self.mat[2][1], self.mat[2][2]],
+            [
+                Angle64::new::<radian>(self.mat[0][0]),
+                Angle64::new::<radian>(self.mat[0][1]),
+                Angle64::new::<radian>(self.mat[0][2]),
+            ],
+            [
+                Angle64::new::<radian>(self.mat[1][0]),
+                Angle64::new::<radian>(self.mat[1][1]),
+                Angle64::new::<radian>(self.mat[1][2]),
+            ],
+            [
+                Angle64::new::<radian>(self.mat[2][0]),
+                Angle64::new::<radian>(self.mat[2][1]),
+                Angle64::new::<radian>(self.mat[2][2]),
+            ],
         ]
     }
 }
 
-impl Default for Transform3D {
+impl Transform3D<f32> {
+    pub fn translation(&self) -> [Length32; 3] {
+        [
+            Length32::new::<meter>(self.mat[0][3]),
+            Length32::new::<meter>(self.mat[1][3]),
+            Length32::new::<meter>(self.mat[2][3]),
+        ]
+    }
+
+    pub fn rotation(&self) -> [[Angle32; 3]; 3] {
+        [
+            [
+                Angle32::new::<radian>(self.mat[0][0]),
+                Angle32::new::<radian>(self.mat[0][1]),
+                Angle32::new::<radian>(self.mat[0][2]),
+            ],
+            [
+                Angle32::new::<radian>(self.mat[1][0]),
+                Angle32::new::<radian>(self.mat[1][1]),
+                Angle32::new::<radian>(self.mat[1][2]),
+            ],
+            [
+                Angle32::new::<radian>(self.mat[2][0]),
+                Angle32::new::<radian>(self.mat[2][1]),
+                Angle32::new::<radian>(self.mat[2][2]),
+            ],
+        ]
+    }
+}
+
+impl<T: Copy + Debug + Default> Default for Transform3D<T> {
     fn default() -> Self {
-        Self { mat: [[0.0; 4]; 4] }
+        Self {
+            mat: [[T::default(); 4]; 4],
+        }
     }
 }
 
@@ -34,8 +86,8 @@ mod faer_integration {
     use super::Transform3D;
     use faer::prelude::*;
 
-    impl From<&Transform3D> for Mat<f64> {
-        fn from(p: &Transform3D) -> Self {
+    impl From<&Transform3D<f64>> for Mat<f64> {
+        fn from(p: &Transform3D<f64>) -> Self {
             let mut mat: Mat<f64> = Mat::zeros(4, 4);
             for r in 0..4 {
                 for c in 0..4 {
@@ -46,7 +98,7 @@ mod faer_integration {
         }
     }
 
-    impl From<Mat<f64>> for Transform3D {
+    impl From<Mat<f64>> for Transform3D<f64> {
         fn from(mat: Mat<f64>) -> Self {
             assert_eq!(mat.nrows(), 4);
             assert_eq!(mat.ncols(), 4);
@@ -67,8 +119,8 @@ mod nalgebra_integration {
     use super::Transform3D;
     use nalgebra::{Isometry3, Matrix3, Matrix4, Rotation3, Translation3, Vector3};
 
-    impl From<&Transform3D> for Isometry3<f64> {
-        fn from(pose: &Transform3D) -> Self {
+    impl From<&Transform3D<f64>> for Isometry3<f64> {
+        fn from(pose: &Transform3D<f64>) -> Self {
             let flat_transform: [f64; 16] = std::array::from_fn(|i| pose.mat[i / 4][i % 4]);
             let matrix = Matrix4::from_row_slice(&flat_transform);
 
@@ -82,7 +134,7 @@ mod nalgebra_integration {
         }
     }
 
-    impl From<Isometry3<f64>> for Transform3D {
+    impl From<Isometry3<f64>> for Transform3D<f64> {
         fn from(iso: Isometry3<f64>) -> Self {
             let matrix = iso.to_homogeneous();
             let transform = std::array::from_fn(|r| std::array::from_fn(|c| matrix[(r, c)]));
@@ -96,8 +148,8 @@ mod glam_integration {
     use super::Transform3D;
     use glam::DAffine3;
 
-    impl From<Transform3D> for DAffine3 {
-        fn from(p: Transform3D) -> Self {
+    impl From<Transform3D<f64>> for DAffine3 {
+        fn from(p: Transform3D<f64>) -> Self {
             let mut aff = DAffine3::IDENTITY;
             aff.matrix3.x_axis.x = p.mat[0][0];
             aff.matrix3.x_axis.y = p.mat[0][1];
@@ -119,7 +171,7 @@ mod glam_integration {
         }
     }
 
-    impl From<DAffine3> for Transform3D {
+    impl From<DAffine3> for Transform3D<f64> {
         fn from(aff: DAffine3) -> Self {
             let mut transform = [[0.0f64; 4]; 4];
 
@@ -157,7 +209,7 @@ mod tests {
 
     #[test]
     fn test_pose_default() {
-        let pose = Transform3D::default();
+        let pose: Transform3D<f32> = Transform3D::default();
         assert_eq!(
             pose.mat, [[0.0; 4]; 4],
             "Default pose should be a zero matrix"
@@ -202,7 +254,7 @@ mod tests {
         };
 
         let iso: Isometry3<f64> = (&pose.clone()).into();
-        let pose_from_iso: Transform3D = iso.into();
+        let pose_from_iso: Transform3D<f64> = iso.into();
 
         assert_eq!(
             pose.mat, pose_from_iso.mat,
@@ -227,7 +279,7 @@ mod tests {
         assert_eq!(pose.mat, orig_pose.mat);
         let aff: DAffine3 = pose.into();
         assert_eq!(aff.translation[0], 5.0);
-        let pose_from_aff: Transform3D = aff.into();
+        let pose_from_aff: Transform3D<f64> = aff.into();
 
         assert_eq!(
             orig_pose.mat, pose_from_aff.mat,
