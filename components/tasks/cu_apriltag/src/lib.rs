ab0378001db0121b1a9f4aca223666877524c0b2
Guillaume Binet <gbin@gootz.net>
2025-03-08 13:53:05 -0600

    Continuing attempting

--- components/tasks/cu_apriltag/src/lib.rs
+++ components/tasks/cu_apriltag/src/lib.rs
@@ -1,5 +1,3 @@
-#![cfg(not(target_os = "windows"))]
-
 // you need a blank line between the cfg and the use
 use apriltag::{Detector, DetectorBuilder, Family, Image, TagParams};
 use apriltag_sys::image_u8_t;
