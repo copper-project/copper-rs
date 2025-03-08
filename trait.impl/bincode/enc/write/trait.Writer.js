ea3ec76688bd12a4182a928f6fc24c70b3b31a12
gbin <gbin@users.noreply.github.com>
2025-03-08 04:07:05 +0000

    deploy: 57ba0fde36881b1cd0a422663e927dea2c1905f8

--- trait.impl/bincode/enc/write/trait.Writer.js
+++ trait.impl/bincode/enc/write/trait.Writer.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cu29_log_runtime",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/bincode/2.0.0-rc.3/bincode/enc/write/trait.Writer.html\" title=\"trait bincode::enc::write::Writer\">Writer</a> for <a class=\"struct\" href=\"cu29_log_runtime/struct.OwningIoWriter.html\" title=\"struct cu29_log_runtime::OwningIoWriter\">OwningIoWriter</a>&lt;W&gt;"]]]]);
+    var implementors = Object.fromEntries([["cu29_log_runtime",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/bincode/2.0.0/bincode/enc/write/trait.Writer.html\" title=\"trait bincode::enc::write::Writer\">Writer</a> for <a class=\"struct\" href=\"cu29_log_runtime/struct.OwningIoWriter.html\" title=\"struct cu29_log_runtime::OwningIoWriter\">OwningIoWriter</a>&lt;W&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[477]}
+//{"start":57,"fragment_lengths":[472]}
