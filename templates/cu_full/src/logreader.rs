3feb1002f877ade1f994ac4c611d38a019f33de6
Yang Zhou <yangzhou.info@gmail.com>
2025-04-15 13:56:13 -0400

    [FEAT] logreader feat for template

--- templates/cu_full/src/logreader.rs
+++ templates/cu_full/src/logreader.rs
@@ -7,6 +7,7 @@ use cu29_export::run_cli;
 // It is used to instruct the log reader how to decode the logs.
 gen_cumsgs!("copperconfig.ron");
 
+#[cfg(feature = "logreader")]
 fn main() {
     run_cli::<CuMsgs>().expect("Failed to run the export CLI");
 }
