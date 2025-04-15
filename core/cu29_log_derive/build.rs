da2138c9b8752efe00a7af6a2c7bf49ae084548b
Yang Zhou <yangzhou.info@gmail.com>
2025-04-15 13:53:42 -0400

    [FIX] add link advapi32 in cu29_log_derive for beta toolchain (#286)
    * [FIX] (cu29_derive) add link lib to advapi32 for windows
    
    * [FIX] link advapi32 for cu29_log_derive

--- core/cu29_log_derive/build.rs
+++ core/cu29_log_derive/build.rs
@@ -1,4 +1,7 @@
 fn main() {
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=advapi32");
+    }
     println!(
         "cargo:rustc-env=LOG_INDEX_DIR={}",
         std::env::var("OUT_DIR").unwrap()
