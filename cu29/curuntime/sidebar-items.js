e3562952728fde0ee7b51c4fd43ab67c4227b8f0
gbin <gbin@users.noreply.github.com>
2025-06-26 20:08:22 +0000

    deploy: cc9a6d1fac117d1cbb5b6836c82bb7d82d9e0892

--- cu29/curuntime/sidebar-items.js
+++ cu29/curuntime/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CuExecutionUnit","CuTaskType"],"fn":["compute_runtime_plan","find_task_type_for_id"],"struct":["CopperContext","CopperListsManager","CuExecutionLoop","CuExecutionStep","CuRuntime","Freezer","FrozenTasksManager"]};
+window.SIDEBAR_ITEMS = {"enum":["CuExecutionUnit","CuTaskType"],"fn":["compute_runtime_plan","find_task_type_for_id"],"struct":["CopperContext","CopperListsManager","CuExecutionLoop","CuExecutionStep","CuRuntime","KeyFrame","KeyFramesManager"]};
