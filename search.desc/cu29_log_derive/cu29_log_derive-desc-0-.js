1a54cd4710ec484ac3f906bea56b0af448580f14
gbin <gbin@users.noreply.github.com>
2025-07-05 12:27:38 +0000

    deploy: 517980aa7bfa22343f0ff66ab92ccb818940541b

--- search.desc/cu29_log_derive/cu29_log_derive-desc-0-.js
+++ search.desc/cu29_log_derive/cu29_log_derive-desc-0-.js
@@ -1,1 +1,1 @@
-searchState.loadedDescShard("cu29_log_derive", 0, "Interns a string For example:")
+searchState.loadedDescShard("cu29_log_derive", 0, "This macro is used to log a debug message with parameters. …\nInterns a string For example:")
