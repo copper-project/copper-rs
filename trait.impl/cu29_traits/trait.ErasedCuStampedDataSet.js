2be87a6819389824fa29797405379e25b4269daa
gbin <gbin@users.noreply.github.com>
2025-07-31 13:21:40 +0000

    deploy: 7cdd9030e71c807e5623d63922cf37acf0911c41

--- trait.impl/cu29_traits/trait.ErasedCuStampedDataSet.js
+++ trait.impl/cu29_traits/trait.ErasedCuStampedDataSet.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cu29",[]],["cu29_runtime",[["impl&lt;P: CopperListTuple&gt; ErasedCuStampedDataSet for <a class=\"struct\" href=\"cu29_runtime/copperlist/struct.CopperList.html\" title=\"struct cu29_runtime::copperlist::CopperList\">CopperList</a>&lt;P&gt;"]]]]);
+    var implementors = Object.fromEntries([["cu29",[]],["cu29_runtime",[["impl&lt;P: <a class=\"trait\" href=\"cu29_traits/trait.CopperListTuple.html\" title=\"trait cu29_traits::CopperListTuple\">CopperListTuple</a>&gt; <a class=\"trait\" href=\"cu29_traits/trait.ErasedCuStampedDataSet.html\" title=\"trait cu29_traits::ErasedCuStampedDataSet\">ErasedCuStampedDataSet</a> for <a class=\"struct\" href=\"cu29_runtime/copperlist/struct.CopperList.html\" title=\"struct cu29_runtime::copperlist::CopperList\">CopperList</a>&lt;P&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[11,235]}
+//{"start":57,"fragment_lengths":[11,481]}
