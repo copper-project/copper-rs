4cd12174296f5c7b838056bf2df66205ada1b188
gbin <gbin@users.noreply.github.com>
2025-09-30 16:04:42 +0000

    deploy: 2ed2ab3aebc5c16bb70d16536f7b9290624b075d

--- trait.impl/core/cmp/trait.Ord.js
+++ trait.impl/core/cmp/trait.Ord.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cu29_clock",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>"]]],["cu29_export",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"cu29_export/enum.ExportFormat.html\" title=\"enum cu29_export::ExportFormat\">ExportFormat</a>"]]],["cu29_log",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"cu29_log/enum.CuLogLevel.html\" title=\"enum cu29_log::CuLogLevel\">CuLogLevel</a>"]]]]);
+    var implementors = Object.fromEntries([["cu29_clock",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"cu29_clock/struct.Instant.html\" title=\"struct cu29_clock::Instant\">Instant</a>"]]],["cu29_export",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"cu29_export/enum.ExportFormat.html\" title=\"enum cu29_export::ExportFormat\">ExportFormat</a>"]]],["cu29_log",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"cu29_log/enum.CuLogLevel.html\" title=\"enum cu29_log::CuLogLevel\">CuLogLevel</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[272,276,261]}
+//{"start":57,"fragment_lengths":[519,276,261]}
--- trait.impl/core/ops/arith/trait.Sub.js
+++ trait.impl/core/ops/arith/trait.Sub.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["cu29_clock",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>"]]]]);
+    var implementors = Object.fromEntries([["cu29_clock",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"cu29_clock/struct.Instant.html\" title=\"struct cu29_clock::Instant\">Instant</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/nightly/core/time/struct.Duration.html\" title=\"struct core::time::Duration\">Duration</a>&gt; for <a class=\"struct\" href=\"cu29_clock/struct.Instant.html\" title=\"struct cu29_clock::Instant\">Instant</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[285]}
+//{"start":57,"fragment_lengths":[957]}
