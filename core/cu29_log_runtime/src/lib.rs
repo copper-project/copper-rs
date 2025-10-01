13e5aa45fc48f9b34cdbae3a1a4d4bce51c535bb
Guillaume Binet <gbin@gootz.net>
2025-10-01 11:26:30 -0500

    no std port for cu29-log-runtime (#452)

--- core/cu29_log_runtime/src/lib.rs
+++ core/cu29_log_runtime/src/lib.rs
@@ -2,11 +2,6 @@
 #[cfg(not(feature = "std"))]
 extern crate alloc;
 
-use bincode::config::Configuration;
-use bincode::enc::write::Writer;
-use bincode::enc::Encode;
-use bincode::enc::{Encoder, EncoderImpl};
-use bincode::error::EncodeError;
 use cu29_clock::RobotClock;
 use cu29_log::CuLogEntry;
 #[allow(unused_imports)]
@@ -14,20 +9,41 @@ use cu29_log::CuLogLevel;
 use cu29_traits::{CuResult, WriteStream};
 use log::Log;
 
-#[cfg(debug_assertions)]
-use {cu29_log::format_logline, std::collections::HashMap, std::sync::RwLock};
+#[cfg(not(feature = "std"))]
+mod imp {
+    pub use alloc::boxed::Box;
+    pub use spin::once::Once as OnceLock;
+    pub use spin::Mutex;
+    pub use spin::RwLock;
+}
 
-use std::fmt::{Debug, Formatter};
-use std::fs::File;
-use std::io::{BufWriter, Write};
-use std::path::PathBuf;
-use std::sync::{Mutex, OnceLock};
+#[cfg(feature = "std")]
+mod imp {
+    pub use bincode::config::Configuration;
+    pub use bincode::enc::write::Writer;
+    pub use bincode::enc::Encode;
+    pub use bincode::enc::Encoder;
+    pub use bincode::enc::EncoderImpl;
+    pub use bincode::error::EncodeError;
+    pub use std::fmt::{Debug, Formatter};
+    pub use std::fs::File;
+    pub use std::io::{BufWriter, Write};
+    pub use std::path::PathBuf;
+    pub use std::sync::{Mutex, OnceLock};
+    #[cfg(debug_assertions)]
+    pub use {cu29_log::format_logline, std::collections::HashMap, std::sync::RwLock};
+}
 
+use imp::*;
+
+#[allow(dead_code)] // for no_std
 #[derive(Debug)]
 struct DummyWriteStream;
 
 impl WriteStream<CuLogEntry> for DummyWriteStream {
+    #[allow(unused_variables)] // for no_std
     fn log(&mut self, obj: &CuLogEntry) -> CuResult<()> {
+        #[cfg(feature = "std")]
         eprintln!("Pending logs got cut: {obj:?}");
         Ok(())
     }
@@ -66,22 +82,34 @@ impl LoggerRuntime {
         // If WRITER is already initialized, update the inner value.
         // This should only be useful for unit testing.
         if let Some((writer, _)) = WRITER.get() {
+            #[cfg(not(feature = "std"))]
+            let mut writer_guard = writer.lock();
+            #[cfg(feature = "std")]
             let mut writer_guard = writer.lock().unwrap();
             *writer_guard = Box::new(destination);
         } else {
+            #[cfg(not(feature = "std"))]
+            WRITER.call_once(|| (Mutex::new(Box::new(destination)), clock));
+            #[cfg(feature = "std")]
             WRITER
                 .set((Mutex::new(Box::new(destination)), clock))
                 .unwrap();
         }
         #[cfg(debug_assertions)]
         if let Some(logger) = extra_text_logger {
-            *EXTRA_TEXT_LOGGER.write().unwrap() = Some(Box::new(logger) as Box<dyn Log>);
+            #[cfg(not(feature = "std"))]
+            let mut extra_text_logger = EXTRA_TEXT_LOGGER.write();
+            #[cfg(feature = "std")]
+            let mut extra_text_logger = EXTRA_TEXT_LOGGER.write().unwrap();
+            *extra_text_logger = Some(Box::new(logger) as Box<dyn Log>);
         }
 
         runtime
     }
 
     pub fn flush(&self) {
+        // no op in no_std TODO(gbin): check if it will be needed in no_std at some point.
+        #[cfg(feature = "std")]
         if let Some((writer, _clock)) = WRITER.get() {
             if let Ok(mut writer) = writer.lock() {
                 if let Err(err) = writer.flush() {
@@ -99,6 +127,8 @@ impl LoggerRuntime {
 impl Drop for LoggerRuntime {
     fn drop(&mut self) {
         self.flush();
+        // Assume on no-std that there is no buffering. TODO(gbin): check if this hold true.
+        #[cfg(feature = "std")]
         if let Some((mutex, _clock)) = WRITER.get() {
             if let Ok(mut writer_guard) = mutex.lock() {
                 // Replace the current WriteStream with a DummyWriteStream
@@ -118,9 +148,15 @@ pub fn log(entry: &mut CuLogEntry) -> CuResult<()> {
     }
     let (writer, clock) = d.unwrap();
     entry.time = clock.now();
+
+    #[cfg(not(feature = "std"))]
+    writer.lock().log(entry)?;
+
+    #[cfg(feature = "std")]
     if let Err(err) = writer.lock().unwrap().log(entry) {
         eprintln!("Failed to log data: {err}");
     }
+
     // This is only for debug builds with standard textual logging implemented.
     #[cfg(debug_assertions)]
     {
@@ -134,6 +170,7 @@ pub fn log(entry: &mut CuLogEntry) -> CuResult<()> {
 /// This version of log is only compiled in debug mode
 /// This allows a normal logging framework to be bridged.
 #[cfg(debug_assertions)]
+#[cfg(feature = "std")]
 pub fn log_debug_mode(
     entry: &mut CuLogEntry,
     format_str: &str, // this is the missing info at runtime.
@@ -194,11 +231,14 @@ pub fn log_debug_mode(
 }
 
 // This is an adaptation of the Iowriter from bincode.
+
+#[cfg(feature = "std")]
 pub struct OwningIoWriter<W: Write> {
     writer: BufWriter<W>,
     bytes_written: usize,
 }
 
+#[cfg(feature = "std")]
 impl<W: Write> OwningIoWriter<W> {
     pub fn new(writer: W) -> Self {
         Self {
@@ -219,6 +259,7 @@ impl<W: Write> OwningIoWriter<W> {
     }
 }
 
+#[cfg(feature = "std")]
 impl<W: Write> Writer for OwningIoWriter<W> {
     #[inline(always)]
     fn write(&mut self, bytes: &[u8]) -> Result<(), EncodeError> {
@@ -234,11 +275,13 @@ impl<W: Write> Writer for OwningIoWriter<W> {
 }
 
 /// This allows this crate to be used outside of Copper (ie. decoupling it from the unifiedlog.
+#[cfg(feature = "std")]
 pub struct SimpleFileWriter {
     path: PathBuf,
     encoder: EncoderImpl<OwningIoWriter<File>, Configuration>,
 }
 
+#[cfg(feature = "std")]
 impl SimpleFileWriter {
     pub fn new(path: &PathBuf) -> CuResult<Self> {
         let file = std::fs::OpenOptions::new()
@@ -258,12 +301,14 @@ impl SimpleFileWriter {
     }
 }
 
+#[cfg(feature = "std")]
 impl Debug for SimpleFileWriter {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "SimpleFileWriter for path {:?}", self.path)
     }
 }
 
+#[cfg(feature = "std")]
 impl WriteStream<CuLogEntry> for SimpleFileWriter {
     #[inline(always)]
     fn log(&mut self, obj: &CuLogEntry) -> CuResult<()> {
