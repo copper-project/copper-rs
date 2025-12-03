2b222d83871659deb4b7b608c3acb845d56168f4
Guillaume Binet <gbin@gootz.net>
2025-12-03 14:53:58 -0600

    pass on scaffolding: firmware compilation

--- examples/cu_basic_fc/src/main.rs
+++ examples/cu_basic_fc/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
-}
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+#[allow(unused_imports)]
+use defmt_rtt as _;
+#[allow(unused_imports)]
+use panic_probe as _;
+
+use buddy_system_allocator::LockedHeap as Heap;
+use rp235x_hal as hal;
+
+#[unsafe(link_section = ".start_block")]
+#[used]
+static IMAGE_DEF: hal::block::ImageDef = hal::block::ImageDef::secure_exe();
+
+#[global_allocator]
+static ALLOC: Heap<32> = Heap::empty();
