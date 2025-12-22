5bc88c04e513bcc6a5c44621d1b966b8d1c0528c
gbin <gbin@users.noreply.github.com>
2025-12-22 22:30:16 +0000

    deploy: 2deb5992a76f74bb9c768d39e528bf7ab8874dfa

---
+++ trait.impl/cu_bincode/enc/write/trait.Writer.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["cu29_log_runtime",[["impl&lt;W: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Write.html\" title=\"trait std::io::Write\">Write</a>&gt; <a class=\"trait\" href=\"https://docs.rs/cu-bincode/2.0.1/cu_bincode/enc/write/trait.Writer.html\" title=\"trait cu_bincode::enc::write::Writer\">Writer</a> for <a class=\"struct\" href=\"cu29_log_runtime/struct.OwningIoWriter.html\" title=\"struct cu29_log_runtime::OwningIoWriter\">OwningIoWriter</a>&lt;W&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[483]}
