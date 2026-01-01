64e93e55070c52e57a38757eff39462bb764220c
gbin <gbin@users.noreply.github.com>
2026-01-01 00:58:43 +0000

    deploy: d00e3b2c6c76cfbbd0f23eec864846301e00739b

---
+++ trait.impl/core/hash/trait.Hash.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["cu29_rendercfg",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"cu29_rendercfg/struct.EdgeGroupKey.html\" title=\"struct cu29_rendercfg::EdgeGroupKey\">EdgeGroupKey</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[297]}
---
+++ trait.impl/layout/core/format/trait.RenderBackend.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["cu29_rendercfg",[["impl RenderBackend for <a class=\"struct\" href=\"cu29_rendercfg/struct.NullBackend.html\" title=\"struct cu29_rendercfg::NullBackend\">NullBackend</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[178]}
