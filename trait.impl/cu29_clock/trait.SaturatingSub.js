f2bfe50f9b9ec3b2ed805182e5e87e0d5ac57cc6
makeecat <makeecat@users.noreply.github.com>
2026-01-06 19:25:50 +0000

    deploy: be7ac3840ee6a731d5b951360bcc4e45db68e6cd

---
+++ trait.impl/cu29_clock/trait.SaturatingSub.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["cu29",[]],["cu29_clock",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[11,18]}
