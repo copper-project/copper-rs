f6d4486de5cca129f3f218328ef6f6419691113e
Guillaume Binet <gbin@gootz.net>
2026-01-13 10:10:31 -0600

    FC: Added the optical flow sensor. Put it in its own mission as it is not mandatory to fly  (#647)
    * MSP: Adds sensor -> FC decoding
    
    * MICOAIRH743: maps the UART4 as a resource.
    
    * logging demo
    
    * format cleanup
    
    * fmt
    
    * put the visual flow on its own mission only
    
    * new_with -> new
    
    * fmt

---
+++ examples/cu_flight_controller/src/tasks/flow_msp.rs
@@ -0,0 +1,45 @@
+use super::*;
+use cu_msp_bridge::MspRequestBatch;
+use cu_msp_lib::structs::MspRequest;
+
+pub struct FlowMspLogger;
+
+impl Freezable for FlowMspLogger {}
+
+impl CuSinkTask for FlowMspLogger {
+    type Input<'m> = CuMsg<MspRequestBatch>;
+    type Resources<'r> = ();
+
+    fn new(_config: Option<&ComponentConfig>, _resources: Self::Resources<'_>) -> CuResult<Self>
+    where
+        Self: Sized,
+    {
+        Ok(Self)
+    }
+
+    fn process<'i>(&mut self, _clock: &RobotClock, input: &Self::Input<'i>) -> CuResult<()> {
+        let Some(batch) = input.payload() else {
+            return Ok(());
+        };
+
+        for request in batch.iter() {
+            match request {
+                MspRequest::MspSensorOpticFlow(flow) => {
+                    debug!(
+                        "                                                                              msp flow q={} dx={} dy={}",
+                        flow.quality, flow.motion_x, flow.motion_y
+                    );
+                }
+                MspRequest::MspSensorRangefinder(range) => {
+                    debug!(
+                        "msp range q={} dist_mm={}",
+                        range.quality, range.distance_mm
+                    );
+                }
+                _ => {}
+            }
+        }
+
+        Ok(())
+    }
+}
