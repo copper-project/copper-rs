1815e8760704f5bbb2d40db65761800d8b27e780
Guillaume Binet <gbin@gootz.net>
2026-01-13 07:25:29 -0600

    fmt

--- examples/cu_flight_controller/src/tasks/flow_msp.rs
+++ examples/cu_flight_controller/src/tasks/flow_msp.rs
@@ -10,10 +10,7 @@ impl CuSinkTask for FlowMspLogger {
     type Input<'m> = CuMsg<MspRequestBatch>;
     type Resources<'r> = ();
 
-    fn new(
-        _config: Option<&ComponentConfig>,
-        _resources: Self::Resources<'_>,
-    ) -> CuResult<Self>
+    fn new(_config: Option<&ComponentConfig>, _resources: Self::Resources<'_>) -> CuResult<Self>
     where
         Self: Sized,
     {
