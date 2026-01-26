301c61a15e556d0b6e3dffe3ca0931bb3e2ef612
gbin <gbin@users.noreply.github.com>
2026-01-26 19:35:10 +0000

    deploy: ce76456fbc22decfa4374c57f1e2f3b562a5ad71

--- api/cu29_log_runtime/sidebar-items.js
+++ api/cu29_log_runtime/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["log","log_debug_mode","structured_log_bytes_total"],"static":["EXTRA_TEXT_LOGGER"],"struct":["LoggerRuntime","NullLog","OwningIoWriter","SimpleFileWriter"]};
+window.SIDEBAR_ITEMS = {"fn":["format_message_only","log","log_debug_mode","register_live_log_listener","structured_log_bytes_total","unregister_live_log_listener"],"struct":["LoggerRuntime","NullLog","OwningIoWriter","SimpleFileWriter"],"type":["LiveLogListener"]};
