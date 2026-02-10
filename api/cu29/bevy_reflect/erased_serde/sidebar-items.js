a6b7431da8a631d3d991119e8a3f3d1e2773b296
gbin <gbin@users.noreply.github.com>
2026-02-10 00:04:29 +0000

    deploy: d30882cad43b4851e122d8a6dcd0ba104a10a662

--- api/cu29/bevy_reflect/erased_serde/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":["deserialize","serialize"],"macro":["serialize_trait_object"],"struct":["Error"],"trait":["Deserializer","Serialize","Serializer"],"type":["Result"]};
--- api/trait.impl/bevy_reflect/from_reflect/trait.FromReflect.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["cu29",[]],["cu29_clock",[["impl FromReflect for <a class=\"struct\" href=\"cu29_clock/struct.CuDuration.html\" title=\"struct cu29_clock::CuDuration\">CuDuration</a>",0]]],["cu29_runtime",[]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[11,162,20]}
