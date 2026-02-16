79f6d0b3544686d470cfb380c97d416ad6b10e7f
Camille Bordignon <camille.bordignon@konboi.one>
2026-02-16 12:05:58 +0100

    fix: ROS2 zenoh attachement format
    Serialization format has changed.
    https://github.com/ros2/rmw_zenoh/pull/601

--- components/bridges/cu_ros2_bridge/src/attachment.rs
+++ components/bridges/cu_ros2_bridge/src/attachment.rs
@@ -4,11 +4,8 @@ use zenoh_ext::ZSerializer;
 
 pub fn encode_attachment(sequence_number: u64, clock: &RobotClock, zid: &ZenohId) -> ZBytes {
     let mut serializer = ZSerializer::new();
-    serializer.serialize("sequence_number".to_string());
     serializer.serialize(sequence_number);
-    serializer.serialize("source_timestamp".to_string());
     serializer.serialize(clock.now().as_nanos());
-    serializer.serialize("source_gid");
     serializer.serialize(zid.to_le_bytes());
     serializer.finish()
 }
