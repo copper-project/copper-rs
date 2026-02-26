3106e069f8cbfb0d2dc79399ca446be1c7a3ddf4
Guillaume Binet <gbin@gootz.net>
2026-02-26 16:07:05 -0600

    cu_consolemon: keep branch scoped to monitor graph fixes

--- examples/cu_flight_controller/src/tasks/monitor.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(all(feature = "sim", not(feature = "firmware")))]
-pub type FlightMonitor = cu_consolemon::CuConsoleMon;
-
-#[cfg(any(feature = "firmware", not(feature = "sim")))]
-pub type FlightMonitor = cu_logmon::CuLogMon;
