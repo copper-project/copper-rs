f785df524b0a5fd2529efc2a9e9912ab86fbe2d9
Guillaume Binet <gbin@gootz.net>
2026-03-01 10:40:35 -0600

    FC: Move the default sim to a city (#874)
    + relaxed the rate (they were very conservative for the real quad)

--- examples/cu_flight_controller/src/sim/rc_tester.rs
+++ examples/cu_flight_controller/src/sim/rc_tester.rs
@@ -6,6 +6,8 @@ use rc_joystick::RcJoystick;
 use std::env;
 use std::io;
 use std::process;
+use std::thread;
+use std::time::Duration;
 
 fn main() -> io::Result<()> {
     if cfg!(not(feature = "sim")) {
@@ -22,5 +24,60 @@ fn main() -> io::Result<()> {
         e
     })?;
 
-    joystick.print_loop()
+    let bindings = joystick.axis_bindings();
+    println!("Using RC joystick: {}", joystick.device_name());
+    println!(
+        "Axis bindings: roll={:?} pitch={:?} yaw={:?} throttle={:?} sa={:?} sb={:?} sc={:?}",
+        bindings.roll,
+        bindings.pitch,
+        bindings.yaw,
+        bindings.throttle,
+        bindings.knob_sa,
+        bindings.knob_sb,
+        bindings.knob_sc
+    );
+    let frame = joystick.current_frame();
+    println!(
+        "Initial frame: roll {:+.2} pitch {:+.2} yaw {:+.2} throttle {:+.2} sa {:+.2} sb {:+.2} sc {:+.2}",
+        frame.roll,
+        frame.pitch,
+        frame.yaw,
+        frame.throttle,
+        frame.knob_sa,
+        frame.knob_sb,
+        frame.knob_sc
+    );
+    println!("Waiting for RC joystick events...");
+    loop {
+        match joystick.next_frame()? {
+            Some(frame) => {
+                let aux_values = frame
+                    .aux
+                    .iter()
+                    .enumerate()
+                    .map(|(i, a)| format!("aux{}:{:+.2}", i + 1, a.value))
+                    .collect::<Vec<_>>()
+                    .join(" ");
+                let switch_values = frame
+                    .switches
+                    .iter()
+                    .map(|s| format!("{}:{}", s.name, if s.on { "on" } else { "off" }))
+                    .collect::<Vec<_>>()
+                    .join(" ");
+                println!(
+                    "roll {:+.2} pitch {:+.2} yaw {:+.2} throttle {:+.2} sa {:+.2} sb {:+.2} sc {:+.2} | {} | {}",
+                    frame.roll,
+                    frame.pitch,
+                    frame.yaw,
+                    frame.throttle,
+                    frame.knob_sa,
+                    frame.knob_sb,
+                    frame.knob_sc,
+                    aux_values,
+                    switch_values
+                );
+            }
+            None => thread::sleep(Duration::from_millis(10)),
+        }
+    }
 }
