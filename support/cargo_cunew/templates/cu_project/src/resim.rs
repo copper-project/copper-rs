ae8ae59d871b29b9a047d2448b2625ff5f51638f
Guillaume Binet <gbin@gootz.net>
2026-05-16 14:14:09 -0500

    fix + test the resim worksapce issue (#1099)

--- support/cargo_cunew/templates/cu_project/src/resim.rs
+++ support/cargo_cunew/templates/cu_project/src/resim.rs
@@ -22,6 +22,7 @@ type ReplayBuildCallback =
     for<'a> fn(
         &'a ReplayCopperList,
         RobotClock,
+        RobotClockMock,
     ) -> Box<dyn for<'z> FnMut(default::SimStep<'z>) -> SimOverride + 'a>;
 type ReplayTimeExtractor = fn(&ReplayCopperList) -> Option<CuTime>;
 
@@ -43,7 +44,8 @@ fn default_callback(_step: default::SimStep<'_>) -> SimOverride {
 
 fn build_callback<'a>(
     copperlist: &'a ReplayCopperList,
-    _clock_for_callbacks: RobotClock,
+    _process_clock: RobotClock,
+    _clock_for_callbacks: RobotClockMock,
 ) -> Box<dyn for<'z> FnMut(default::SimStep<'z>) -> SimOverride + 'a> {
     Box::new(move |step: default::SimStep<'_>| default::recorded_replay_step(step, copperlist))
 }
@@ -114,7 +116,7 @@ fn run_one_shot(log_base: &Path, replay_log_base: &Path) -> CuResult<()> {
             clock_mock.set_value(ts.as_nanos());
         }
 
-        let mut sim_callback = build_callback(&entry, clock.clone());
+        let mut sim_callback = build_callback(&entry, clock.clone(), clock_mock.clone());
         app.run_one_iteration(&mut sim_callback)?;
     }
 
