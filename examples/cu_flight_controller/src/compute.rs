b55d9add8a7c18511d6d6bf1de57196aa6130483
Guillaume Binet <gbin@gootz.net>
2026-07-13 20:28:10 +0200

    fc: Adds the zed dependency [2] (#1185)
    * Split the Quad copter in compute + mcu
    
    * Adds the zed dependency
    
    * fmt
    
    * Remove mistaken flight-controller .agents symlinks

--- examples/cu_flight_controller/src/compute.rs
+++ examples/cu_flight_controller/src/compute.rs
@@ -51,7 +51,12 @@ mod tasks {
     }
 }
 
+#[cfg(not(feature = "end2end"))]
 #[copper_runtime(config = "multi_copper.ron", subsystem = "compute")]
+struct ComputeApp {}
+
+#[cfg(feature = "end2end")]
+#[copper_runtime(config = "multi_copper_end2end.ron", subsystem = "compute")]
 struct ComputeApp {}
 
 const LOG_SLAB_SIZE: Option<usize> = Some(64 * 1024 * 1024);
