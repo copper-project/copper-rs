7fcb473e4da684dea25e5523801deda2dd9f6e37
Guillaume Binet <gbin@gootz.net>
2026-08-31 11:01:27 -0500

    Report remote debug log indexing progress

--- core/cu29_unifiedlog/src/memmap.rs
+++ core/cu29_unifiedlog/src/memmap.rs
@@ -919,6 +919,28 @@ impl UnifiedLogRead for MmapUnifiedLoggerRead {
             }
         }
     }
+
+    fn raw_skip_section(&mut self) -> CuResult<SectionHeader> {
+        if self.current_reading_position >= self.current_mmap_buffer.len() {
+            self.next_slab().map_err(|e| {
+                CuError::new_with_cause("Failed to read next slab, is the log complete?", e)
+            })?;
+        }
+
+        let header = self.read_section_header().map_err(|error| {
+            CuError::new_with_cause(
+                &format!(
+                    "Could not read a sections header: {}/{}:{}",
+                    self.base_file_path.as_os_str().to_string_lossy(),
+                    self.current_slab_index,
+                    self.current_reading_position,
+                ),
+                error,
+            )
+        })?;
+        self.current_reading_position += header.offset_to_next_section as usize;
+        Ok(header)
+    }
 }
 
 impl MmapUnifiedLoggerRead {
